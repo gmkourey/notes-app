c52b22999ef070a5db29c61aa06265bd43675ca3
eswilson <eswilson4@gmail.com>
2018-10-05 00:31:03 -0700

    binding functions in app.js not necessary, commented out for now

--- src/App.js
+++ src/App.js
@@ -9,101 +9,6 @@ import RightDrawer from './components/RightDrawer/RightDrawer';
 import Typography from '@material-ui/core/Typography';
 // import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 
-// const styles = theme => ({
-// const theme = createMuiTheme({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   appFrame: {
-//     height: '100vh',
-//     zIndex: 1,
-//     overflow: 'hidden',
-//     position: 'relative',
-//     display: 'flex',
-//     width: '100%',
-//   }
-// });
-// const drawerWidth = 240;
-
-// const styles = theme => ({
-//   root: {
-//     flexGrow: 1,
-//   },
-//   appFrame: {
-//     height: '100vh', // Important
-//     zIndex: 1,
-//     overflow: 'hidden',
-//     position: 'relative',
-//     display: 'flex',
-//     width: '100%',
-//   },
-//   appBar: {
-//     zIndex: theme.zIndex.drawer + 1,
-//     position: 'absolute',
-//     transition: theme.transitions.create(['margin', 'width'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//   },
-//   appBarShift: {
-//     width: `calc(100% - ${drawerWidth}px)`,
-//     transition: theme.transitions.create(['margin', 'width'], {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   'appBarShift-left': {
-//     marginLeft: drawerWidth,
-//   },
-//   'appBarShift-right': {
-//     marginRight: drawerWidth,
-//   },
-//   menuButton: {
-//     marginLeft: 12,
-//     marginRight: 20,
-//   },
-//   hide: {
-//     display: 'none',
-//   },
-//   drawerPaper: {
-//     position: 'relative',
-//     width: drawerWidth,
-//   },
-//   drawerHeader: {
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'flex-end',
-//     padding: '0 8px',
-//     ...theme.mixins.toolbar,
-//   },
-//   content: {
-//     flexGrow: 1,
-//     backgroundColor: theme.palette.background.default,
-//     padding: theme.spacing.unit * 3,
-//     transition: theme.transitions.create('margin', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//   },
-//   'content-left': {
-//     marginLeft: -drawerWidth,
-//   },
-//   'content-right': {
-//     marginRight: -drawerWidth,
-//   },
-//   contentShift: {
-//     transition: theme.transitions.create('margin', {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   'contentShift-left': {
-//     marginLeft: 0,
-//   },
-//   'contentShift-right': {
-//     marginRight: 0,
-//   },
-// });
 
 class App extends Component {
   constructor() {
@@ -112,7 +17,7 @@ class App extends Component {
       leftOpen: false,
       rightOpen: false
     };
-    this.handleLeftDrawer = this.handleLeftDrawer.bind(this);
+    // this.handleLeftDrawer = this.handleLeftDrawer.bind(this);
     // this.handleRightDrawer = this.handleRightDrawer.bind(this);
   }
 
