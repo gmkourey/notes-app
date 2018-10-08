a881c4b13fe1cc2c8b42700d17feb0b788302beb
Cale <youremailhere@whatever.com>
2018-10-08 13:25:19 -0700

    updating notearea

--- src/components/NoteArea.js
+++ src/components/NoteArea.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Editor, EditorState} from 'draft-js';
+import { Editor, EditorState, convertToRaw, convertFromRaw } from 'draft-js';
+import axios from 'axios';
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {};
     this.onChange = (editorState) => {
+      const contentState = editorState.getCurrentContent();
+      this.saveContent(contentState);
       this.setState({editorState}); 
-      console.log('');
     }
     this.setEditor = (editor) => {
       this.editor = editor;
@@ -22,9 +24,27 @@ class MyEditor extends React.Component {
 
   componentDidMount() {
     this.focusEditor();
+    //api call to see if note exists - props.children for "Note_uid" from the leftpane when they click note?
+    .then(rawContent => {
+      if (rawContent) {
+        this.setState({ editorState:
+        EditorState.createWithContent(convertFromRaw(rawContent)) })
+      } else {
+        this.setState({ editorState: EdtiorState.createEmpty() })
+      }
+    })
+  }
+
+  saveContent = (content) => {
+    return axios.post('api/note', 
   }
 
   render() {
+    if (!this.state.editorState) {
+      return (
+        <h3 className="loading">Loading...</h3>
+      );
+    }
     return (
       <div style={styles.editor} onClick={this.focusEditor}>
         <Editor
@@ -37,6 +57,7 @@ class MyEditor extends React.Component {
   }
 } 
 
+//pull this out later into standalone css?
 const styles = {
   editor: {
     border: '1px solid gray',
