de70a91153e5c3764fa42bd6f3be093cdcab551e
Cale <youremailhere@whatever.com>
2018-10-10 12:05:17 -0700

    added babel

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 // var express = require("express");
-// var mongoose = require("mongoose");
-// var bodyParser = require("body-parser");
-// var routes = require('./routes/api');
-
-// var PORT = 3001;
-
-// var app = express();
-
-// app.use(bodyParser.urlencoded({extended:true}));
-
-// app.use(bodyParser.json());
-
-// app.use(express.static("public"));
-
-// mongoose.connect("mongodb://localhost/notes-app", {useNewUrlParser: true});
-
-// require("./routes/html/html-routes")(app);
-// app.use(routes);
-
-
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log("connected to db")
-// });
-
-// app.listen(PORT, function() {
-//     console.log("App running on port 3001!");
-//   });
-
-
-// pasted slate-automerge
-'use strict';
-
 const app = require('express')();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var routes = require('./routes/api');
+
+var PORT = 3001;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/notes-app", {useNewUrlParser: true});
+
+require("./routes/html/html-routes")(app);
+
+app.use(routes);
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected to db")
+  });
+
+  // app.listen(PORT, function() {
+  //     console.log("App running on port 3001!");
+  //   });
+
+
+  // // pasted slate-automerge
+// // 'use strict';
+
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
 const Automerge = require("automerge")
@@ -149,6 +148,6 @@ io.on('connection', function(socket) {
 });
 
 
-http.listen(5000, function() {
-  console.log('listening on *:5000');
+http.listen(PORT, function() {
+  console.log('listening on *:3000');
 });
