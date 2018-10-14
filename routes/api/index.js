9b85f12340a429706a5dd28248c7aede1c872453
eswilson <eswilson4@gmail.com>
2018-10-14 15:29:36 -0700

    heroku deploy test 5

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,4 +2,9 @@ var router = require("express").Router();
 var apiRoute = require("./api-routes");
 
 router.use("/api", apiRoute);
+
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"))
+})
+
 module.exports = router;
