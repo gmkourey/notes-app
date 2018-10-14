10dbbae4a39ffceaf1751e9e90bccc8613d3b133
eswilson <eswilson4@gmail.com>
2018-10-14 16:39:02 -0700

    heroku

---
+++ client/build/precache-manifest.13efb42916aef773db3ba31a64359eb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d5a5958da2298a19d4dc",
+    "url": "/static/js/main.d5a5958d.chunk.js"
+  },
+  {
+    "revision": "154028e316dc9279d0e9",
+    "url": "/static/js/1.154028e3.chunk.js"
+  },
+  {
+    "revision": "d5a5958da2298a19d4dc",
+    "url": "/static/css/main.54c24c49.chunk.css"
+  },
+  {
+    "revision": "67419a79caa0c0c3c433b0cd13b74c5c",
+    "url": "/index.html"
+  }
+];
