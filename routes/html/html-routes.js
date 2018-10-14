dbe73c8dd5a2743eeb32b0cae8cbabb9a2a455ee
eswilson <eswilson4@gmail.com>
2018-10-14 15:22:55 -0700

    heroku deploy test 4

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -1,14 +1,14 @@
-var path = require("path");
+// var path = require("path");
+
+// // module.exports = function(app) {
+// //     app.get("/notes", function(req, res) {
+// //         res.sendFile(path.join(__dirname, "../../client/test.html"));
+// //     })
+// // }
 
 // module.exports = function(app) {
-//     app.get("/notes", function(req, res) {
-//         res.sendFile(path.join(__dirname, "../../client/test.html"));
+//     app.get("/", function(req, res) {
+//         // res.status(200).send("Hi testing ok");
+//         res.sendFile(path.join(__dirname, "../client/build/index.html"));
 //     })
 // }
-
-module.exports = function(app) {
-    app.get("/", function(req, res) {
-        // res.status(200).send("Hi testing ok");
-        res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    })
-}
--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(bodyParser.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static("client/build"));
 
 // mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds117061.mlab.com:17061/heroku_7175wr6b`, {useNewUrlParser: true});
 // mongoose.connect(`mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@ds131903.mlab.com:31903/heroku_x35lf8pl`, {useNewUrlParser: true});
@@ -22,7 +23,7 @@ var MONGODB_URI = process.env.MONGODB_URI;
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-require("./routes/html/html-routes")(app);
+// require("./routes/html/html-routes")(app);
 
 app.use(routes);
 
