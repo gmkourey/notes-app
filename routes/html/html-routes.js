fd5fe846f70b417e3e1482d1c3cca4922fdd2b83
eswilson <eswilson4@gmail.com>
2018-10-14 18:05:13 -0700

    heroku

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -2,6 +2,6 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "../../client/test.html"));
+        res.sendFile(path.join(__dirname, "../../client/build/index.html"));
     })
 }
