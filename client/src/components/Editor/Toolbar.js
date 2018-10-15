79137ac47a6980a096eec65fc8d2c76d5ef5d6b8
eswilson <eswilson4@gmail.com>
2018-10-15 03:57:14 -0700

    removed unused toolbar component

--- client/src/components/Editor/Toolbar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Toolbar = (props) => (
-	<div>{props.children}</div>
-);
-
-export default Toolbar;
--- client/src/components/Editor/index.js
+++ client/src/components/Editor/index.js
@@ -1,5 +1,4 @@
 import Bold from "./Bold";
 import Italic from "./Italic";
-import Toolbar from "./Toolbar";
 
-export { Bold, Italic, Toolbar };
+export { Bold, Italic };
