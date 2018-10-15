97aad8a296a0ea5757d5e0bcfb53d25c710a9dbd
eswilson <eswilson4@gmail.com>
2018-10-14 21:41:47 -0700

    heroku

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // else {
-app.use(express.static("public"));
+// app.use(express.static("public"));
 // }
 
 
