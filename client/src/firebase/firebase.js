3d68b6c69cc50a5ce1a36c4293cf9532dca1cb1a
eswilson <eswilson4@gmail.com>
2018-10-15 11:35:35 -0700

    firebase prodConfig fix

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 const prodConfig = {
-    apiKey: YOUR_API_KEY,
-    authDomain: YOUR_AUTH_DOMAIN,
-    databaseURL: YOUR_DATABASE_URL,
-    projectId: YOUR_PROJECT_ID,
-    storageBucket: '',
-    messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+    apiKey: "AIzaSyCBMBQDk44o7BFspQVxjEPfc5uVo2fw4m0",
+    authDomain: "note-app-ucberk.firebaseapp.com",
+    databaseURL: "https://note-app-ucberk.firebaseio.com",
+    projectId: "note-app-ucberk",
+    storageBucket: "note-app-ucberk.appspot.com",
+    messagingSenderId: "888209507310"
   };
   
   const devConfig = {
