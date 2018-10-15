970f0d6a5b2fe90313529bb8d369c191eadf0e00
eswilson <eswilson4@gmail.com>
2018-10-14 18:44:02 -0700

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './components/Page/MainPage';
 import SignUpPage from './components/SignUp';
 import SignInPage from './components/SignIn';
@@ -11,7 +8,7 @@ import AccountPage from './components/Account';
 // import Note from "./components/Note";
 
 import withAuthentication from './components/withAuthentication';
-import * as routes from './constants/routes';
+import routes from './constants/routes';
 
 // currently unused components, shouldn't need them in the end
 // import Navigation from './components/Navigation';
@@ -21,17 +18,14 @@ import * as routes from './constants/routes';
 const App = () =>
   <Router>
     <>
-      {/* <Navigation /> */}
-
-      {/* <Route exact path={routes.LANDING} component={LandingPage} /> */}
-      <Route exact path={routes.LANDING} component={MainPage} />
-      <Route exact path={routes.SIGN_UP} component={SignUpPage} />
-      <Route exact path={routes.SIGN_IN} component={SignInPage} />
-      <Route exact path={routes.PASSWORD_FORGET} component={PasswordForgetPage} />
-      <Route exact path={routes.HOME} component={MainPage} />
-      <Route exact path={routes.ACCOUNT} component={AccountPage} />
-      {/* <Route exact path={routes.NOTE} component={Note}/> */}
-
+      <Switch>
+        <Route exact path={routes.LANDING} component={MainPage} />
+        <Route exact path={routes.SIGN_UP} component={SignUpPage} />
+        <Route exact path={routes.SIGN_IN} component={SignInPage} />
+        <Route exact path={routes.PASSWORD_FORGET} component={PasswordForgetPage} />
+        <Route exact path={routes.HOME} component={MainPage} />
+        <Route exact path={routes.ACCOUNT} component={AccountPage} />
+      </Switch>
     </>
   </Router>
 
