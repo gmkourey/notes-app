ed9db2f2cc7d78d3aa909ef57b39bb93b3babbd8
eswilson <eswilson4@gmail.com>
2018-10-14 18:12:41 -0700

    heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const routes = require('./routes/api');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 // require('dotenv').config()
